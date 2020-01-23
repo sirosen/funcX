e734c78e9ad3ed4fb9a469d5cac83aeb2e3bedc0
Yadu Nand B <yadudoc1729@gmail.com>
2020-01-23 11:23:51 -0600

    Remove throttling limits

--- funcx/sdk/utils/throttling.py
+++ funcx/sdk/utils/throttling.py
@@ -34,7 +34,7 @@ class ThrottledBaseClient(globus_sdk.base.BaseClient):
 
     """
     # Max requests per second, in a 5 second period
-    DEFAULT_MAX_REQUESTS = 10000
+    DEFAULT_MAX_REQUESTS = 100
     # Max size is 2Mb
     DEFAULT_MAX_REQUEST_SIZE = 2 * 2 ** 20
 
