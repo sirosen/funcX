11f3f09485031932e23aef48403b365bda2e3445
Rohan Kumar <therohankumar7@gmail.com>
2020-02-25 16:51:26 -0600

    option to run locally via simple local executor

--- funcx/executors/__init__.py
+++ funcx/executors/__init__.py
@@ -1,3 +1,4 @@
 from funcx.executors.high_throughput.executor import HighThroughputExecutor
+from funcx.executors.local.executor import LocalExecutor
 
-__all__ = ['HighThroughputExecutor']
+__all__ = ['HighThroughputExecutor', 'LocalExecutor']
--- funcx/sdk/scheduler.py
+++ funcx/sdk/scheduler.py
@@ -1,15 +1,31 @@
 import time
+import uuid
 import random
 import logging
 from collections import defaultdict
 from threading import Thread
+from queue import Queue, Empty
+
+try:
+    from termcolor import colored
+except ImportError:
+    def colored(x, *args, **kwargs):
+        return x
 
 from funcx.sdk.client import FuncXClient
+from funcx.executors import LocalExecutor
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter(colored("[SCHEDULER] %(message)s", 'yellow')))
 logger.addHandler(ch)
+
+watchdog_logger = logging.getLogger(__name__ + '_watchdog')
+watchdog_logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter(colored("[WATCHDOG]  %(message)s", 'green')))
+watchdog_logger.addHandler(ch)
 
 
 class timer(object):
@@ -39,7 +55,7 @@ class FuncXScheduler:
         self._fxc.fx_serializer.use_custom('03\n', 'code')
 
         # List of all FuncX endpoints we can execute on
-        self._endpoints = list(set(endpoints)) or []
+        self._endpoints = (list(set(endpoints)) or []) + ['local']
 
         # Average times for each function on each endpoint
         # TODO: this info is too unrealistic to have
@@ -56,6 +72,15 @@ class FuncXScheduler:
                              .format(self.STRATEGIES))
         self.strategy = strategy
 
+        # Start a thread to do local execution
+        self._functions = {}
+        self._local_task_queue = Queue()
+        self._local_result_queue = Queue()
+        self._local_worker_thread = Thread(target=LocalExecutor,
+                                           args=(self._local_task_queue,
+                                                 self._local_result_queue))
+        self._local_worker_thread.start()
+
         # Start a thread to wait for results and record runtimes
         self._watchdog_sleep_time = 0.1  # in seconds
         self._watchdog_thread = Thread(target=self._wait_for_results)
@@ -64,12 +89,14 @@ class FuncXScheduler:
     def add_endpoint(self, endpoint_id):
         self._endpoints.add(endpoint_id)
 
-    def remove_endpoiint(self, endpoint_id):
+    def remove_endpoint(self, endpoint_id):
         self._endpoints.discard(endpoint_id)
 
     def register_function(self, function, *args, **kwargs):
         wrapped_function = timer(function)
-        return self._fxc.register_function(wrapped_function, *args, **kwargs)
+        func_id = self._fxc.register_function(wrapped_function, *args, **kwargs)
+        self._functions[func_id] = wrapped_function
+        return func_id
 
     def run(self, *args, function_id, asynchronous=False, **kwargs):
         endpoint_id = self._choose_best_endpoint(*args,
@@ -81,9 +108,13 @@ class FuncXScheduler:
             'endpoint_id': endpoint_id
         }
 
-        task_id = self._fxc.run(*args, function_id=function_id,
-                                endpoint_id=endpoint_id,
-                                asynchronous=asynchronous, **kwargs)
+        if endpoint_id == 'local':
+            task_id = self._run_locally(*args, function_id=function_id,
+                                        **kwargs)
+        else:
+            task_id = self._fxc.run(*args, function_id=function_id,
+                                    endpoint_id=endpoint_id,
+                                    asynchronous=asynchronous, **kwargs)
 
         logger.debug('Sent function {} to endpoint {} with task_id {}'
                      .format(function_id, endpoint_id, task_id))
@@ -148,28 +179,52 @@ class FuncXScheduler:
             raise NotImplementedError("TODO: implement other stategies")
 
     def _wait_for_results(self):
+        '''Watchdog thread function'''
 
-        logger.info('[WATCHDOG] Thread started')
+        watchdog_logger.info('Thread started')
 
         while True:
             to_delete = set()
+
+            # Check if there are any local results ready
+            while True:
+                try:
+                    local_output = self._local_result_queue.get_nowait()
+                    res = local_output['result']
+                    task_id = local_output['task_id']
+                    watchdog_logger.debug('Got result for task {} from '
+                                          'endpoint local with runtime {}'
+                                          .format(task_id, res['runtime']))
+                    self._update_average(task_id, new_time=res['runtime'])
+                    self._results[task_id] = res['result']
+                    to_delete.add(task_id)
+
+                except Empty:
+                    break
+
             # Convert to list first because otherwise, the dict may throw an
             # exception that its size has changed during iteration. This can
             # happen when new pending tasks are added to the dict.
             for task_id, info in list(self._pending_tasks.items()):
+
+                # Local tasks will come from local results queue
+                if info['endpoint_id'] == 'local':
+                    continue
+
+                # If remote, ask funcX service for result
                 try:
                     res = self._fxc.get_result(task_id)
                 except Exception as e:
                     if not str(e).startswith("Task pending"):
-                        logger.warn('[WATCHDOG] Got unexpected exception:\t{}'
-                                    .format(e))
+                        watchdog_logger.warn('Got unexpected exception:\t{}'
+                                             .format(e))
                         raise
                     continue
 
-                logger.debug('[WATCHDOG] Got result for task {} from '
-                             'endpoint {} with runtime {}'
-                             .format(task_id, info['endpoint_id'],
-                                     res['runtime']))
+                watchdog_logger.debug('Got result for task {} from '
+                                      'endpoint {} with runtime {}'
+                                      .format(task_id, info['endpoint_id'],
+                                              res['runtime']))
                 self._update_average(task_id, new_time=res['runtime'])
                 self._results[task_id] = res['result']
                 to_delete.add(task_id)
@@ -177,6 +232,7 @@ class FuncXScheduler:
                 # Sleep, to prevent being throttled
                 time.sleep(self._watchdog_sleep_time)
 
+            # Stop tracking all tasks which have now returned
             for task_id in to_delete:
                 del self._pending_tasks[task_id]
 
@@ -190,3 +246,16 @@ class FuncXScheduler:
         new_avg = (old_avg * num_times + new_time) / (num_times + 1)
         self._runtimes[function_id][endpoint_id] = new_avg
         self._num_executions[function_id][endpoint_id] += 1
+
+    def _run_locally(self, *args, function_id, **kwargs):
+
+        data = {
+            'function': self._functions[function_id],
+            'args': args,
+            'kwargs': kwargs,
+            'task_id': str(uuid.uuid4())
+        }
+
+        self._local_task_queue.put(data)
+
+        return data['task_id']
